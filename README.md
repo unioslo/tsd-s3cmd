a14a12d60cd4daca5446a53cfd59e1446dc60095
leondutoit <dutoit.leon@gmail.com>
2020-09-03 11:47:47 +0200

    add setup, update readme, scripts, and gitignore

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # tsd-s3cmd
+
 Wrapper for s3cmd, for the TSD API
+
+# Install
+
+```bash
+pip3 install https://github.com/unioslo/tsd-s3cmd/archive/v0.1.0.zip
+```
+
+# Usage
+
+To use the TSD s3 API, use `tsd-s3cmd`:
+
+```bash
+tacl --register
+tsd-s3cmd --guide
+tsd-s3cmd --help
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='tsd-s3cmd',
+    version='0.1.0',
+    description='TSD Wrapper for s3cmd',
+    author='Leon du Toit, Eirik Haatveit',
+    author_email='l.c.d.toit@usit.uio.no',
+    url='https://github.com/unioslo/tsd-s3cmd',
+    scripts=[
+        'scripts/tacl_admin',
+        'scripts/tacl_auth',
+        'scripts/tsd-s3cmd'],
+    install_requires = [
+        'tsd-api-client @ https://github.com/unioslo/tsd-api-client/archive/v2.7.1.tar.gz',
+        's3cmd @ https://github.com/unioslo/s3cmd/archive/v2.1.0-custom-headers.tar.gz#egg=s3cmd-2.1.0-custom-headers'
+    ],
+    python_requires='>=3.6',
+)
