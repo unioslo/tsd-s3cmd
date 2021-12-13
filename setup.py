f616965bba26d475fabcbfa58e18bc3de154168b
Eirik Haatveit <haatveit@uio.no>
2021-12-13 11:23:43 +0000

    Version 0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='tsd-s3cmd',
-    version='0.1.0',
+    version='0.1.2',
     description='TSD Wrapper for s3cmd',
     author='Leon du Toit, Eirik Haatveit',
     author_email='l.c.d.toit@usit.uio.no',
